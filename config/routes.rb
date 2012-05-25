6fad88bec0093e205370c7cfb931f637326789da
Zack Bishop <zack@freckleinteractive.net>
2012-05-25 13:11:47 -0400

    minor updates

--- config/routes.rb
+++ config/routes.rb
@@ -21,21 +21,19 @@ FinishMySideProject2::Application.routes.draw do
     resources :comments
   end
 
-  get "home/index"
+  get "home/index", as: 'index'
 
-  get "home/about"
+  get "home/about", as: 'about'
 
-  get "home/terms_of_use"
+  get "home/terms_of_use", as: 'terms'
 
-  get "home/privacy_policy"
-
-  get "home/side_project"
+  get "home/privacy_policy", as: 'privacy'
 
   get "users/new"
 
   get "users/show"
 
-  get "home/endorse"
+  get "home/endorse", as: 'endorse'
 
 
   #This solves the routes issues to users: http://stackoverflow.com/questions/5136940/undefined-method-user-path
