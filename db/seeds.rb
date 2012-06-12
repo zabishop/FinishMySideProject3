c01a5ac2a7f17880161bcca7c6cef0e052bae409
Zack Bishop <zack@freckleinteractive.net>
2012-06-12 09:52:21 -0400

    Seed database update
    added tag data to seed file
    reworked seed data so that each user has two projects submitted
    created two different tags, red and blue

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,22 +12,35 @@ SideProject.delete_all
 
 Comment.delete_all
 
-for i in 1..20
+for i in 1..10
 
   @my_user = User.create(:name => 'user ' + i.to_s,
               :password => 'password' + i.to_s,
               :profile_pic => 'baby.png',
               :email => 'user' + i.to_s + '@gmail.com')
 
-  @my_side_project = SideProject.create(:project_title => 'project' + i.to_s,
+  @my_side_project1 = SideProject.create(:project_title => 'project' + i.to_s,
                      :description => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                      :image_url => 'project_pic.jpg',
                      :repo_url => 'repo' + i.to_s + '.com',
                      :user => @my_user)
 
+  @my_side_project2 = SideProject.create(:project_title => 'project' + (i + 10).to_s,
+                                         :description => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                                         :image_url => 'project_pic.jpg',
+                                         :repo_url => 'repo' + (i + 10).to_s + '.com',
+                                         :user => @my_user)
+
   Comment.create(:commenter => @my_user,
                  :body => 'i like your project!',
                  :side_project => @my_side_project)
+
+
+  Tag.create(:name => 'red',
+             :side_project => @my_side_project1)
+
+  Tag.create(:name => 'blue',
+             :side_project => @my_side_project2)
 end
 
 
